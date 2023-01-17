9d99dba999a7f8f56ab50d1597f2755b2dad32d7
woo-chang <clllickme@naver.com>
2023-01-18 05:16:03 +0900

    feat: 유저 엔티티, 레포지토리 생성

---
+++ src/main/java/com/example/deploy/domain/user/entity/User.java
@@ -0,0 +1,42 @@
+package com.example.deploy.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", updatable = false, nullable = false)
+  private Long id;
+
+  @Column(name = "name", nullable = false, length = 45)
+  private String name;
+
+  @Column(name = "login_id", nullable = false, unique = true, length = 45)
+  private String loginId;
+
+  @Column(name = "password", nullable = false, length = 512)
+  private String password;
+
+  @Column(name = "introduce", length = 255)
+  private String introduce;
+
+  @Builder
+  public User(String name, String loginId, String password, String introduce) {
+    this.name = name;
+    this.loginId = loginId;
+    this.password = password;
+    this.introduce = introduce;
+  }
+}
